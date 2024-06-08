f72bf348bc924edd5d08a52dc44e49bb3d0db9de
Marvin Johanning <email@marvinjohanning.de>
2024-06-08 23:07:39 +0200

    change design

---
+++ template.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>title here</title>
+
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/fonts.css">
+</head>
+
+<?php include "$_SERVER[DOCUMENT_ROOT]/header.php"; ?>
+
+<body>
+    <main class="container">
+        <section class="box-shadow">
+            <!-- For picture at top of site
+            <figure class="profile-picture-container">
+                <img src="/assets/images/profile.jpg" alt="Profile Picture"/>
+                <figcaption>Me at the library of the University of Bielefeld</figcaption>
+            </figure>
+            -->
+
+            <section>
+                <h2>Section Title 1</h2>
+                <div class="justified">
+                </div>
+            </section>
+
+            <section>
+                <h2>Section Title 2</h2>
+                </div>
+            </section>
+
+            <!-- For adding links
+            <section class="links">
+                <h2>Social links and other websites of mine</h2>
+                <ul>
+                    <li><a href="_blank" target="_blank">example.com</a></li>
+                </ul>
+            </section>
+            -->
+
+        </section>
+    </main>
+
+    <?php include "$_SERVER[DOCUMENT_ROOT]/footer.php"; ?>
+
+</body>
+</html>
+
